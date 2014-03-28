a25634d27aba72cfb3df282f6a7b568a78a7f81c
Carly Heibel <heibelcj@rose-hulman.edu>
2014-03-28 19:39:18 -0400

    trying to add all modified files in the folder

--- SettlersOfCatan/SettlersOfCatan/GameScreen.cs
+++ SettlersOfCatan/SettlersOfCatan/GameScreen.cs
@@ -14,7 +14,106 @@ namespace SettlersOfCatan
     {
         public GameScreen()
         {
+
             InitializeComponent();
+
+            setupButtons();
+
         }
+
+        private List<System.Windows.Forms.Button> settlementButtons = new List<System.Windows.Forms.Button>();
+        private void setupButtons(){
+
+            Panel p = new Panel();
+            p.Location = new Point(0, 0);
+            p.Size = new Size(900, 900);
+            this.Controls.Add(p);
+
+            for(int i = 0; i < 5; i++){
+                Button b = new Button();
+                b.BackColor = Color.Red;
+                b.Location = new Point(0, i*10);
+                settlementButtons.Add(b);
+                p.Controls.Add(b);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            //button1.BackColor = Color.Blue;
+            //button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.Green;
+            button2.Height = 30;
+            button2.Width = 20;
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ItemToBuildComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox myComboBox = (ComboBox)sender;
+            if (myComboBox.SelectedIndex == 0)
+            {
+                // Show all available road buttons
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+            }
+            else if (myComboBox.SelectedIndex == 1)
+            {
+                // Show all available settlement buttons
+            }
+            else if (myComboBox.SelectedIndex == 2)
+            {
+                // enable all relevant current settlement buttons to be changed into cities
+            }
+        }
+
+        private void showOnlyOpenRoadButtons()
+        {
+            // Disable all city/settlement buttons
+
+            // Hide all unused city/settlement buttons
+
+            // Show and enable all unused road buttons
+
+
+
+
+        }
+
+        private void IntersectionButton_Click(object sender, EventArgs e)
+        {
+            Button theButton = (Button)sender;
+
+
+            if (theButton.Width == 30 && theButton.BackColor != System.Drawing.Color.White)
+            {
+                theButton.Text = "*";
+                theButton.ForeColor = Color.White;
+                theButton.Enabled = false;
+            }
+
+            theButton.BackColor = Color.Orange;
+        }
+
+        //private void AddControls(int cNumber) 
+        //{ 
+            
+        //}
+
+        //}
+
+
     }
 }
