5cecb756eff59fd911449d3ac532a1c473838983
Nathan Cheung <cheungnj@rose-hulman.edu>
2014-04-04 16:31:20 -0400

    Added a Computer class that extends Player as well as a ComputerTest class

---
+++ SettlersOfCatan/ClassLibrary1/ComputerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SettlersOfCatan;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace ClassLibrary1
+{
+    [TestFixture()]
+    class ComputerTest
+    {
+        [Test()]
+        public void TestComputerInitializesCorrectly()
+        {
+            var target = new Computer();
+            Assert.NotNull(target);
+        }
+
+        [Test()]
+        public void TestComputerConstructsNameAndColorCorrectly()
+        {
+            String computerName = "computer1";
+            Color computerColor = Color.White;
+            var world = new World();
+            Computer computer1 = new Computer(computerName, computerColor, world);
+            Assert.AreEqual(computerColor, computer1.getColor());
+            Assert.AreEqual(computerName, computer1.getName());
+        }
+
+    }
+}
---
+++ SettlersOfCatan/SettlersOfCatan/Computer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Reflection;
+
+namespace SettlersOfCatan
+{
+    public class Computer : Player
+    {
+        
+        public Computer() : base()
+        {
+        }
+
+        public Computer(String computerName, Color computerColor, World world) : 
+            base(computerName, computerColor, world)
+        {
+        }
+
+
+    }
+}
