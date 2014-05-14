968236c33d612bdb80c966292ee6839e178aa71b
Carly Heibel <heibelcj@rose-hulman.edu>
2014-05-13 22:12:45 -0400

    started a Port class

---
+++ SettlersOfCatan/SettlersOfCatan/Port.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettlersOfCatan
+{
+    public class Port
+    {
+        String resourceType;
+        int tradeAmount;
+
+
+        public Port(String r, int amount)
+        {
+            this.resourceType = r;
+            this.tradeAmount = amount;
+        }
+
+
+
+    }
+}
